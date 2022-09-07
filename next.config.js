cf9120d2f63fca0ffbd9ef9780b926fd91bac735
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2022-09-07 22:04:26 +0530

    update github actions & nextjs config to support link prefixing

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /**
  * @type {import('next').NextConfig}
  */
+const repoNameURIPrefix =
+  process.env.NODE_ENV == "production" ? process.env.REPO_NAME : "";
 
 module.exports = {
+  assetPrefix: repoNameURIPrefix,
+  env: {
+    linkPrefix: repoNameURIPrefix,
+  },
   experimental: {
     images: {
       unoptimized: true,
